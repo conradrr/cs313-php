977ec2c6029b566c41eca24b15a7ddb6af746cb5
Conrad <rifeconrad@gmail.com>
2018-05-01 16:41:43 -0600

    testPHP.php

---
+++ web/testPHP.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head><title>PHP Practice</title></head>
+<body>
+	<p>Test PHP</p>
+	<?php
+	for ($i = 1; $i <= 10; $i++)
+		echo "<div id=div$i></div>"
+	
+	
+	
+	?>
+</body>
+</html>
