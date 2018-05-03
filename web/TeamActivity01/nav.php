99867a278aef2f405d83046374c2959f2e88e05b
Conrad <rifeconrad@gmail.com>
2018-05-03 16:05:17 -0600

    TeamActivity\

--- web/TeamActivity01/nav.php
+++ web/TeamActivity01/nav.php
@@ -2,14 +2,12 @@
     <div class="container-fluid">
         <div class="navbar-header">
             <a href="#" class="navbar-brand"></a>
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><i class="fa fa-bars"></i></button>
         </div>
         <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="index">Home</a></li>                    
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Home Page</a></li>
-                <li><a href="#">Login</a></li>
+            <ul class="nav navbar-nav navbar-right">                  
+                <li id="aboutUsActivity"><a href="about-us.php">About Us</a></li>
+                <li id="homeActivity"><a href="home.php">Home Page</a></li>
+                <li id="loginActivity"><a href="login.php">Login</a></li>
             </ul>
         </div>
     </div>
