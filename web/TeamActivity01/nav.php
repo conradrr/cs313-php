6317b1013563960afb7de04eacd46e919afe22e0
Conrad <rifeconrad@gmail.com>
2018-05-03 16:27:20 -0600

    TeamActivity01\

--- web/TeamActivity01/nav.php
+++ web/TeamActivity01/nav.php
@@ -5,9 +5,9 @@
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">                  
-                <li class="<?php if($pages=='about'){echo 'active';}?>"><a href="about-us.php">About Us</a></li>
-                <li class="<?php if($pages=='home'){echo 'active';}?>"><a href="home.php">Home Page</a></li>
-                <li class="<?php if($pages=='login'){echo 'active';}?>"><a href="login.php">Login</a></li>
+                <li class="<?php if($pages=='about'){echo 'class="active"';}?>"><a href="about-us.php">About Us</a></li>
+                <li class="<?php if($pages=='home'){echo 'class="active"';}?>"><a href="home.php">Home Page</a></li>
+                <li class="<?php if($pages=='login'){echo 'class="active"';}?>"><a href="login.php">Login</a></li>
             </ul>
         </div>
     </div>
