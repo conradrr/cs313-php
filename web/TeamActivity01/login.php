8b3b4fc8e0777f4a076137a0cda17ec6d3deaf56
Conrad <rifeconrad@gmail.com>
2018-05-03 16:29:05 -0600

    TeamActivity01\

--- web/TeamActivity01/login.php
+++ web/TeamActivity01/login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<?php $page = 'login';include 'nav.php';?>
+		<?php $pages = 'login';include 'nav.php';?>
 		<link rel="stylesheet" type="text/css" href="nav.css">
 		<title>Login</title>
 	</head>
