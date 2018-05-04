733962173b94b7bce4cfa00ad08105fcdfd4255a
Conrad <rifeconrad@gmail.com>
2018-05-04 16:05:23 -0600

    The MouseOver should be done

--- web/index.php
+++ web/index.php
@@ -10,7 +10,7 @@
 		<h1>Conrad's Home Page</h1>
 		<div id="aboutMeBox" onmouseover="displayInfo()" onmouseout="retractInfo()">About Me...</div>
 		<div id="aboutMeDisplay">
-			I was born in Palm Springs, CA and raised in Dayton, OH. I have a wife (Desma) and a five month old girl (Amelia). Currently, I'm studying Computer Science, while my wife is studying to become a Physical Therapist. My only interest, besides working with computers, is spending time with my family. I'd say it doesn't matter what we do together, as long as we are together.
+			I was born in Palm Springs, CA and raised in Dayton, OH. <br>I have a wife (Desma) and a five month old girl (Amelia).<br>Currently, I'm studying Computer Science, while my wife is studying to become a Physical Therapist.<br>My only interest, besides working with computers, is spending time with my family.<br>I'd say it doesn't matter what we do together, as long as we are together.
 		</div>
 	</body>
 </html>
