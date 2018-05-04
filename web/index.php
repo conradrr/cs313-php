7d113012f366aa2893fe74eb8a83c4adf3af30e5
Conrad <rifeconrad@gmail.com>
2018-05-04 14:43:52 -0600

    Initial Home Page Test

--- web/index.php
+++ web/index.php
@@ -1,5 +1,10 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Conrad's Home Page</title>
+	</head>
+	<body>
+		<h1>Conrad's Home Page</h1>
+		<div id="navbar"><p> Home Page </p><p> Assignments </p></div>
+	</body>
+</html>
