80a42e9c2723d66ecb8b6890e02067e1847cfef6
Conrad <rifeconrad@gmail.com>
2018-05-04 16:00:35 -0600

    added onmouseover for display information

--- web/index.js
+++ web/index.js
@@ -1,11 +1,9 @@
 function displayInfo()
 {
-	document.getElementById("aboutMeBox").style.visibility = "collapse";
    document.getElementById("aboutMeDisplay").style.visibility = "visible";
 }
 
 function retractInfo()
 {
-   document.getElementById("aboutMeBox").style.visibility = "visible";
    document.getElementById("aboutMeDisplay").style.visibility = "collapse";
 }
