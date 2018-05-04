91d106ddcda8595d155694b26e97a46976297aee
Conrad <rifeconrad@gmail.com>
2018-05-04 14:51:08 -0600

    Linking pages to home page

--- web/index.php
+++ web/index.php
@@ -5,6 +5,6 @@
 	</head>
 	<body>
 		<h1>Conrad's Home Page</h1>
-		<div id="navbar"><a> Home Page</a><a> Assignments</a></div>
+		<div id="navbar"><a href="index.php"> Home Page</a><a href="assignments.php"> Assignments</a></div>
 	</body>
 </html>
