5a2b3a536201e4a440aeef6d32d82722d22f83c4
Conrad <rifeconrad@gmail.com>
2018-05-04 16:30:10 -0600

    pictures added

--- web/index.php
+++ web/index.php
@@ -10,7 +10,13 @@
 		<h1>Conrad's Home Page</h1>
 		<div id="aboutMeBox" onmouseover="displayInfo()" onmouseout="retractInfo()">About Me...</div>
 		<div id="aboutMeDisplay">
-			I was born in Palm Springs, CA and raised in Dayton, OH. <br>I have a wife (Desma) and a five month old girl (Amelia).<br>Currently, I'm studying Computer Science, while my wife is studying to become a Physical Therapist.<br>My only interest, besides working with computers, is spending time with my family.<br>I'd say it doesn't matter what we do together, as long as we are together.
+			I was born in Palm Springs, CA and raised in Dayton, OH. <br>I've been married for over two years to my wife Desma and we have a five month old daughter named Amelia.<br>Currently, I'm studying Computer Science, while my wife is studying to become a Physical Therapist.<br>My only interest, besides working with computers, is spending time with my family.<br>I'd say it doesn't matter what we do together, as long as we are together.
+		</div>
+		<div id="photosBox" onmouseover="displayPhotos()" onmouseout="retractPhotos()">Photos of Our Family</div>
+		<div id ="photosDisplay">
+			<img src="familyPicture1.jpg" alt="Family Picture">
+			<img src="familyPicture2.jpg" alt="Amelia Rose">
+			<img src="familyPicture3.jpg" alt="Wedding Day">
 		</div>
 	</body>
 </html>
