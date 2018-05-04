14cc7c97d60ea678fbda70a0b13a0ce68c8467a1
Conrad <rifeconrad@gmail.com>
2018-05-04 15:09:37 -0600

    Navbar Finished?

--- web/navbar.php
+++ web/navbar.php
@@ -1,9 +1,6 @@
 <link rel="stylesheet" type="text/css" href="index.css">
-<header class="navbar navbar-default navbar-static-top">
+<header class="navbar">
     <div class="container-fluid">
-        <div class="navbar-header">
-            <a href="#" class="navbar-brand"></a>
-        </div>
         <div class="navbar-collapse collapse">                
         		<a class="<?php if($page=='home'){echo 'active';}?>" href="index.php">Home Page</a>
 				<a class="<?php if($page=='assignment'){echo 'active';}?>" href="assignments.php">Assignments</a>
