8958d6bfdd4e1a23f9cefa5085914d151b1fde6a
Conrad <rifeconrad@gmail.com>
2018-05-04 14:59:16 -0600

    Linking pages to home page

--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<?php $page = 'home'; include 'navbar.php';?>
+		<link rel="stylesheet" type="text/css" href="index.css">
 		<title>Conrad's Home Page</title>
 	</head>
 	<body>
---
+++ web/navbar.php
@@ -0,0 +1,14 @@
+<header class="navbar navbar-default navbar-static-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="#" class="navbar-brand"></a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">                  
+                <li class="<?php if($page=='about'){echo 'active';}?>"><a href="about-us.php">About Us</a></li>
+                <li class="<?php if($page=='home'){echo 'active';}?>"><a href="home.php">Home Page</a></li>
+                <li class="<?php if($page=='login'){echo 'active';}?>"><a href="login.php">Login</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
